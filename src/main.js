83b7e37dfa2e7ac6560ee6f1d10aa1d6a3490e21
Tatiana Penkova <tatianapenkova@gmail.com>
2020-04-24 12:21:35 +0300

    main.js updated

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-// eslint-disable-next-line import/newline-after-import
-// eslint-disable-next-line import/no-absolute-path
-import '/Users/User/Desktop/git/ajs-homeworks/ajs-homework-3-1/src/style.css';
-
 function playerHealth(index) {
   if (index > 50) {
     return 'healthy';
